d6851c39f1dd52747da1cef5b8be637aa99e2691
Sukhdip <=>
2020-06-10 23:34:25 -0700

    updated code - working mu law compression

--- main.c
+++ main.c
@@ -2,20 +2,28 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdint.h>
 
+// **********************************************************************
 // DEFINE STATEMENTS
-#define BIAS 33
-#define MAX 0x1FDF
+// **********************************************************************
+#define BITS_PER_SAMPLE 8
+#define MULAW_MAX 0x1FFF
+#define MULAW_BIAS 33
 
+
+// **********************************************************************
 // FUNCTION DECLARATIONS
-int get_16_bit_integer(char buffer[2]);
+// **********************************************************************
+int16_t bytes_to_int16(unsigned char *buffer);
 
-int signum(int sample);
-
-int magnitude(int sample);
+int8_t u_law_encode(int16_t number);
 
 char codeword_compression(unsigned int sample_magnitude, int sign);
 
+// **********************************************************************
+// STRUCT
+// **********************************************************************
 struct wav_header_struct {
     unsigned char riff[4];                        // RIFF string
     unsigned char overall_size[4];                // overall size of file in bytes
@@ -32,7 +40,9 @@ struct wav_header_struct {
     unsigned char data_size[4];                   // NumSamples * NumChannels * BitsPerSample/8 - size of the next chunk that will be read
 };
 
+// **********************************************************************
 // GLOBAL VARIABLES
+// **********************************************************************
 FILE *inputfile;
 FILE *outputfile;
 char *input_file_name;
@@ -40,12 +50,15 @@ char *output_file_name;
 
 struct wav_header_struct wavHeaderStruct;
 
-// MAIN
+// **********************************************************************
+// MAIN FUNCTION
+// **********************************************************************
 int main(int argc, char **argv) {
-    char cwd[1024];
-    char file_bytes[2];
-    unsigned char bytes_4[4];
-    unsigned char bytes_2[4];
+    char current_directory[1024];
+    unsigned char byte_buffer_2[2];
+    unsigned char byte_buffer_4[4];
+    int input_data;
+    int8_t codeword;
 
     input_file_name = (char *) malloc(sizeof(char) * 1024);
     output_file_name = (char *) malloc(sizeof(char) * 1024);
@@ -55,8 +68,8 @@ int main(int argc, char **argv) {
         exit(1);
     }
 
-    if (getcwd(cwd, sizeof(cwd)) != NULL) {
-        strcpy(input_file_name, cwd);
+    if (getcwd(current_directory, sizeof(current_directory)) != NULL) {
+        strcpy(input_file_name, current_directory);
         if (argc < 2) {
             printf("No input file specified!\n");
             exit(1);
@@ -74,21 +87,19 @@ int main(int argc, char **argv) {
         printf("Error opening input file!\n");
         exit(1);
     }
+
     outputfile = fopen(output_file_name, "wb");
     if (outputfile == NULL) {
         printf("Error writing output file!\n");
         exit(1);
     }
-    
-    // **********************************************************************
-    // RIFF (CHUNK ID)
-    // **********************************************************************
+
+
+    // Riff - does not change
     fread(wavHeaderStruct.riff, sizeof(wavHeaderStruct.riff), 1, inputfile);
     fwrite(wavHeaderStruct.riff, sizeof(wavHeaderStruct.riff), 1, outputfile);
 
-    // **********************************************************************
-    // OVERALL SIZE (CHUNK SIZE)
-    // **********************************************************************
+    // Overall Size (ChunkSize) - the new size must be calculated and written
     fread(wavHeaderStruct.overall_size, sizeof(wavHeaderStruct.overall_size), 1, inputfile);
     unsigned int overall_size = wavHeaderStruct.overall_size[0]
                                 | (wavHeaderStruct.overall_size[1] << 8)
@@ -97,65 +108,45 @@ int main(int argc, char **argv) {
     unsigned int new_overall_size = overall_size + 8 - 44;
     new_overall_size = new_overall_size / 2;
     new_overall_size = new_overall_size + 44 - 8;
+    byte_buffer_4[0] = (new_overall_size >> 24) & 0xFF;
+    byte_buffer_4[1] = (new_overall_size >> 16) & 0xFF;
+    byte_buffer_4[2] = (new_overall_size >> 8) & 0xFF;
+    byte_buffer_4[3] = new_overall_size & 0xFF;
+    fwrite(&byte_buffer_4[3], 1, 1, outputfile);
+    fwrite(&byte_buffer_4[2], 1, 1, outputfile);
+    fwrite(&byte_buffer_4[1], 1, 1, outputfile);
+    fwrite(&byte_buffer_4[0], 1, 1, outputfile);
 
-    bytes_4[0] = (new_overall_size >> 24) & 0xFF;
-    bytes_4[1] = (new_overall_size >> 16) & 0xFF;
-    bytes_4[2] = (new_overall_size >> 8) & 0xFF;
-    bytes_4[3] = new_overall_size & 0xFF;
-    fwrite(&bytes_4[3], 1, 1, outputfile);
-    fwrite(&bytes_4[2], 1, 1, outputfile);
-    fwrite(&bytes_4[1], 1, 1, outputfile);
-    fwrite(&bytes_4[0], 1, 1, outputfile);
-//    fwrite(wavHeaderStruct.overall_size, sizeof(wavHeaderStruct.overall_size), 1, outputfile);
-
-
-    // **********************************************************************
-    // WAVE (FORMAT)
-    // **********************************************************************
+    // Wave (Format) - does not change
     fread(wavHeaderStruct.wave, sizeof(wavHeaderStruct.wave), 1, inputfile);
     fwrite(wavHeaderStruct.wave, sizeof(wavHeaderStruct.wave), 1, outputfile);
 
-    // **********************************************************************
-    // FMT
-    // **********************************************************************
+    // Format Chunk Marker (Subchunk1ID) - does not change
     fread(wavHeaderStruct.fmt_chunk_marker, sizeof(wavHeaderStruct.fmt_chunk_marker), 1, inputfile);
     fwrite(wavHeaderStruct.fmt_chunk_marker, sizeof(wavHeaderStruct.fmt_chunk_marker), 1, outputfile);
 
-    // **********************************************************************
-    // Length of Format (Subchunk1Size)
-    // **********************************************************************
+    // Length of Format (Subchunk1Size) - may need to be changed but for now it is left alone
     fread(wavHeaderStruct.length_of_fmt, sizeof(wavHeaderStruct.length_of_fmt), 1, inputfile);
-//    bytes_4[0] = 16;
-//    bytes_4[1] = '\0';
-//    bytes_4[2] = '\0';
-//    bytes_4[3] = '\0';
-//    fwrite(&bytes_4[0], 1, 1, outputfile);
-//    fwrite(&bytes_4[1], 1, 1, outputfile);
-//    fwrite(&bytes_4[2], 1, 1, outputfile);
-//    fwrite(&bytes_4[3], 1, 1, outputfile);
+/*    byte_buffer_4[0] = 16; byte_buffer_4[1] = '\0';
+    byte_buffer_4[2] = '\0'; byte_buffer_4[3] = '\0';
+    fwrite(&byte_buffer_4[0], 1, 1, outputfile); fwrite(&byte_buffer_4[1], 1, 1, outputfile);
+    fwrite(&byte_buffer_4[2], 1, 1, outputfile); fwrite(&byte_buffer_4[3], 1, 1, outputfile);*/
     fwrite(wavHeaderStruct.length_of_fmt, sizeof(wavHeaderStruct.length_of_fmt), 1, outputfile);
 
-    // **********************************************************************
-    // Audio Format (1 PCM | 6 ALAW | 7 MuLaw)
-    // **********************************************************************
+    // Audio Format Type: 1=PCM; 6=ALAW; 7=MuLaw
     fread(wavHeaderStruct.format_type, sizeof(wavHeaderStruct.format_type), 1, inputfile);
-    bytes_2[0] = 7;
-    bytes_2[1] = '\0';
-    fwrite(&bytes_2[0], 1, 1, outputfile);
-    fwrite(&bytes_2[1], 1, 1, outputfile);
-//    fwrite(wavHeaderStruct.format_type, sizeof(wavHeaderStruct.format_type), 1, outputfile);
+    byte_buffer_2[0] = 7;
+    byte_buffer_2[1] = '\0';
+    fwrite(&byte_buffer_2[0], 1, 1, outputfile);
+    fwrite(&byte_buffer_2[1], 1, 1, outputfile);
 
-    // **********************************************************************
-    // Number of Channels
-    // **********************************************************************
+    // Number of Channels - record the value but do not change it
     fread(wavHeaderStruct.num_channels, sizeof(wavHeaderStruct.num_channels), 1, inputfile);
     unsigned int num_channels = wavHeaderStruct.num_channels[0]
                                 | (wavHeaderStruct.num_channels[1] << 8);
     fwrite(wavHeaderStruct.num_channels, sizeof(wavHeaderStruct.num_channels), 1, outputfile);
 
-    // **********************************************************************
-    // SampleRate
-    // **********************************************************************
+    // Sample Rate - Record the value but do not change it
     fread(wavHeaderStruct.sample_rate, sizeof(wavHeaderStruct.sample_rate), 1, inputfile);
     unsigned int sample_rate = wavHeaderStruct.sample_rate[0]
                                | (wavHeaderStruct.sample_rate[1] << 8)
@@ -163,82 +154,53 @@ int main(int argc, char **argv) {
                                | (wavHeaderStruct.sample_rate[3] << 24);
     fwrite(wavHeaderStruct.sample_rate, sizeof(wavHeaderStruct.sample_rate), 1, outputfile);
 
-    // **********************************************************************
-    // Byte Rate: SampleRate * NumChannels * BitsPerSample/8
-    // **********************************************************************
+    // Byte Rate = (SampleRate * NumChannels * BitsPerSample) / 8
     fread(wavHeaderStruct.byte_rate, sizeof(wavHeaderStruct.byte_rate), 1, inputfile);
-    unsigned int byte_rate = (sample_rate * num_channels * 8) / 8;
-    bytes_4[0] = (byte_rate >> 24) & 0xFF;
-    bytes_4[1] = (byte_rate >> 16) & 0xFF;
-    bytes_4[2] = (byte_rate >> 8) & 0xFF;
-    bytes_4[3] = byte_rate & 0xFF;
-    fwrite(&bytes_4[3], 1, 1, outputfile);
-    fwrite(&bytes_4[2], 1, 1, outputfile);
-    fwrite(&bytes_4[1], 1, 1, outputfile);
-    fwrite(&bytes_4[0], 1, 1, outputfile);
-//    fwrite(wavHeaderStruct.byte_rate, sizeof(wavHeaderStruct.byte_rate), 1, outputfile);
+    unsigned int byte_rate = (sample_rate * num_channels * BITS_PER_SAMPLE) / 8;
+    byte_buffer_4[0] = (byte_rate >> 24) & 0xFF;
+    byte_buffer_4[1] = (byte_rate >> 16) & 0xFF;
+    byte_buffer_4[2] = (byte_rate >> 8) & 0xFF;
+    byte_buffer_4[3] = byte_rate & 0xFF;
+    fwrite(&byte_buffer_4[3], 1, 1, outputfile);
+    fwrite(&byte_buffer_4[2], 1, 1, outputfile);
+    fwrite(&byte_buffer_4[1], 1, 1, outputfile);
+    fwrite(&byte_buffer_4[0], 1, 1, outputfile);
 
-    // **********************************************************************
-    // Block Align: NumChannels * BitsPerSample / 8
-    // **********************************************************************
+    // Block Align = (NumChannels * BitsPerSample) / 8
     fread(wavHeaderStruct.block_align, sizeof(wavHeaderStruct.block_align), 1, inputfile);
-    unsigned int block_align = (num_channels * 8) / 8;
-    bytes_2[0] = (block_align >> 8) & 0xFF;
-    bytes_2[1] = block_align & 0xFF;
-    fwrite(&bytes_2[1], 1, 1, outputfile);
-    fwrite(&bytes_2[0], 1, 1, outputfile);
-//    fwrite(wavHeaderStruct.block_align, sizeof(wavHeaderStruct.block_align), 1, outputfile);
+    unsigned int block_align = (num_channels * BITS_PER_SAMPLE) / 8;
+    byte_buffer_2[0] = (block_align >> 8) & 0xFF;
+    byte_buffer_2[1] = block_align & 0xFF;
+    fwrite(&byte_buffer_2[1], 1, 1, outputfile);
+    fwrite(&byte_buffer_2[0], 1, 1, outputfile);
 
-    // **********************************************************************
-    // BITS PER SAMPLE
-    // **********************************************************************
+    // Bits per sample = 8
     fread(wavHeaderStruct.bits_per_sample, sizeof(wavHeaderStruct.bits_per_sample), 1, inputfile);
-    bytes_2[0] = 8;
-    bytes_2[1] = '\0';
-    fwrite(&bytes_2[0], 1, 1, outputfile);
-    fwrite(&bytes_2[1], 1, 1, outputfile);
-//    fwrite(wavHeaderStruct.bits_per_sample, sizeof(wavHeaderStruct.bits_per_sample), 1, outputfile);
+    byte_buffer_2[0] = BITS_PER_SAMPLE;
+    byte_buffer_2[1] = '\0';
+    fwrite(&byte_buffer_2[0], 1, 1, outputfile);
+    fwrite(&byte_buffer_2[1], 1, 1, outputfile);
 
-    // **********************************************************************
-    // DATA CHUNK HEADER
-    // **********************************************************************
+    // Data chunk header - does not change
     fread(wavHeaderStruct.data_chunk_header, sizeof(wavHeaderStruct.data_chunk_header), 1, inputfile);
     fwrite(wavHeaderStruct.data_chunk_header, sizeof(wavHeaderStruct.data_chunk_header), 1, outputfile);
 
-    // **********************************************************************
-    // DATA SIZE
-    // **********************************************************************
+    // Data size - calcuate the size of the remaining input_data
     fread(wavHeaderStruct.data_size, sizeof(wavHeaderStruct.data_size), 1, inputfile);
     int chunk_header = new_overall_size + 8 - 44;
-    bytes_4[0] = (chunk_header >> 24) & 0xFF;
-    bytes_4[1] = (chunk_header >> 16) & 0xFF;
-    bytes_4[2] = (chunk_header >> 8) & 0xFF;
-    bytes_4[3] = chunk_header & 0xFF;
-    fwrite(&bytes_4[3], 1, 1, outputfile);
-    fwrite(&bytes_4[2], 1, 1, outputfile);
-    fwrite(&bytes_4[1], 1, 1, outputfile);
-    fwrite(&bytes_4[0], 1, 1, outputfile);
-//    fwrite(wavHeaderStruct.data_size, sizeof(wavHeaderStruct.data_size), 1, outputfile);
+    byte_buffer_4[0] = (chunk_header >> 24) & 0xFF;
+    byte_buffer_4[1] = (chunk_header >> 16) & 0xFF;
+    byte_buffer_4[2] = (chunk_header >> 8) & 0xFF;
+    byte_buffer_4[3] = chunk_header & 0xFF;
+    fwrite(&byte_buffer_4[3], 1, 1, outputfile);
+    fwrite(&byte_buffer_4[2], 1, 1, outputfile);
+    fwrite(&byte_buffer_4[1], 1, 1, outputfile);
+    fwrite(&byte_buffer_4[0], 1, 1, outputfile);
 
-
-    int sign = 0;
-    int number;
-    int i = 0;
-    while (fread(file_bytes, 1, 2, inputfile) == 2) {
-        number = get_16_bit_integer(file_bytes);
-        if (number < 0) {
-            number = -number;
-            sign = 0;
-        } else {
-            sign = 1;
-        }
-        if (number >= MAX) {
-            number = MAX;
-        }
-        number += BIAS;
-        unsigned char compressed_codeword = codeword_compression(number, sign);
-        fwrite(&compressed_codeword, 1, 1, outputfile);
-        i++;
+    while (fread(byte_buffer_2, 1, 2, inputfile) == 2) {
+        input_data = bytes_to_int16(byte_buffer_2);
+        codeword = u_law_encode(input_data);
+        fwrite(&codeword, 1, 1, outputfile);
     }
 
     fclose(inputfile);
@@ -249,30 +211,33 @@ int main(int argc, char **argv) {
 }
 
 // HELPER FUNCTIONS
-int get_16_bit_integer(char buffer[2]) {
+int16_t bytes_to_int16(unsigned char *buffer) {
     unsigned char bit_one = buffer[0];
     unsigned char bit_two = buffer[1];
-    int number = ((int) bit_two << 8) | (int) bit_one;
+    int16_t number = ((int) bit_two << 8) | (int) bit_one;
     return number;
 }
 
-
-int signum(int sample) {
-    if (sample < 0)
-        return (0); /* sign is ’0’ for negative samples */
-    else
-        return (1); /* sign is ’1’ for positive samples */
-}
-
-
-int magnitude(int sample) {
-    if (sample < 0) {
-        sample = -sample;
+int8_t u_law_encode(int16_t number) {
+    uint16_t mask = 0x1000;
+    uint8_t sign = 0;
+    uint8_t position = 12;
+    uint8_t lsb = 0;
+    if (number < 0) {
+        number = -number;
+        sign = 0x80;
     }
-    return (sample);
+    number += MULAW_BIAS;
+    if (number > MULAW_MAX) {
+        number = MULAW_MAX;
+    }
+    for (; ((number & mask) != mask && position >= 5); mask >>= 1, position--);
+    lsb = (number >> (position - 4)) & 0x0f;
+    return (~(sign | ((position - 5) << 4) | lsb));
 }
 
 
+/*
 char codeword_compression(unsigned int sample_magnitude,
                           int sign) {
     int chord, step;
@@ -326,4 +291,4 @@ char codeword_compression(unsigned int sample_magnitude,
         return ((char) codeword_tmp);
     }
     return '\0';
-}
+}*/
